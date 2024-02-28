9d393b30d965ba745a442e70bb9a22bf90e70d74
Benicio Enriquez <benicio.m.enriquez@gmail.com>
2024-02-28 15:49:06 -0500

    Removed junk

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Lighting
-var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
 scene.add(ambientLight);
 
 var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
@@ -23,21 +23,9 @@ var hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x000000, 0.5);
 scene.add(hemisphereLight);
 
 // Init Physics
-const world = new CANNON.World();
-world.gravity.set(0, -9.81, 0);
-world.broadphase = new CANNON.NaiveBroadphase();
-world.solver.iterations = 10;
-let physObjs = [];
 
-// Ground
-// const groundShape = new CANNON.Plane();
-// const groundBody = new CANNON.Body({ mass: 0 });
-// groundBody.addShape(groundShape);
-// groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI / 2);
-// groundBody.isPin = false;
-// world.addBody(groundBody);
 
-// Add Ball
+// Add Player
 const p1Geometry = new THREE.SphereGeometry(0.5, 32, 32);
 const p1Material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const p1 = new THREE.Mesh(p1Geometry, p1Material);
@@ -45,43 +33,9 @@ const ballShape = new CANNON.Sphere(0.5);
 p1.userData.physicsBody = new CANNON.Body({ mass: 1 });
 p1.userData.physicsBody.addShape(ballShape);
 p1.userData.physicsBody.position.set(0, 0, 0);
+world.addBody(p1.userData.physicsBody);
+physObjs.push(p1);
 scene.add(p1);
-//physObjs.push(p1);
-//world.addBody(p1.userData.physicsBody);
-
-// loader.load(
-//     './ball.glb',
-//     function (gltf) {
-//         ball = gltf.scene.children[0];
-//         ball.castShadow = true;
-
-//         ball.scale.set(0.5, 0.5, 0.5);
-
-//         const ballShape = new CANNON.Sphere(0.5);
-//         ball.userData.physicsBody = new CANNON.Body({ mass: 10 });
-//         ball.userData.physicsBody.addShape(ballShape);
-//         ball.userData.physicsBody.position.set(0, 1, -10);
-//         ball.userData.physicsBody.isPin = false;
-
-//         function handleCollision(event) {
-//             console.log(event);
-//             if (event.body.isPin && !event.body.hasCollided) {
-//                 score++;
-//                 document.getElementById('overlay').textContent = 'Score: ' + score;
-//                 event.body.hasCollided = true;
-//             }
-//         }
-
-//         ball.userData.physicsBody.addEventListener('collide', handleCollision);
-
-//         ball.position.copy(ball.userData.physicsBody.position);
-//         world.addBody(ball.userData.physicsBody);
-//         ball.visible = false;
-//         scene.add(ball);
-//     }
-// );
-
-// Handle Stuff
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -106,13 +60,12 @@ function animate() {
     requestAnimationFrame(animate);
 
     // Update physics
-    world.step(1 / 60);
+    // world.step(1 / 60);
 
-    physObjs.forEach((o, index) => {
-        const p = o.userData.physicsBody;
-        o.position.copy(p.position);
-        o.quaternion.copy(p.quaternion);
-    });
+    // physObjs.forEach((o, index) => {
+    //     o.position.copy(o.userData.physicsBody.position);
+    //     o.quaternion.copy(o.userData.physicsBody.quaternion);
+    // });
 
     renderer.render(scene, camera);
 } 
